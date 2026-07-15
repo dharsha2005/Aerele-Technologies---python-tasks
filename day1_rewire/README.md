f785f242769a2e27d0ed587371b092fdc2aa37a7
dharsha2005 <baladharshan1972@gmail.com>
2026-07-15 17:22:48 +0530

    task1

---
+++ day1_rewire/README.md
@@ -0,0 +1,6 @@
+1. python3 -m venv venv
+   source venv/bin/activate
+
+2. Install package: pip install requests
+
+3. Requirements install : pip freeze > requirements.txt
---
+++ day1_rewire/exercise.py
@@ -0,0 +1,34 @@
+'''
+Write a Small Script Using List Comprehension and Context Manager
+'''
+students = ["Alice", "Bob", "Charlie", "David", "Emma"]
+marks = [95, 78, 88, 67, 91]
+passed_students = [
+    name
+    for name, mark in zip(students, marks)
+    if mark >= 80
+]
+with open("numbers.txt", "w") as file:
+    file.write("Passed Students\n")
+    file.write("-----------------\n")
+    for student in passed_students:
+        file.write(student + "\n")
+print("Result file created successfully.")
+
+'''
+Java-style code
+'''
+students = ["Alice", "Bob", "Charlie"]
+marks = [95, 78, 88]
+
+for i in range(len(students)):
+    print(students[i] + " scored " + str(marks[i]))
+
+'''
+Rewrite It the Pythonic Way
+'''
+students = ["Alice", "Bob", "Charlie"]
+marks = [95, 78, 88]
+
+for index, (student, mark) in enumerate(zip(students, marks), start=1):
+    print(f"{index}. {student} scored {mark}")
