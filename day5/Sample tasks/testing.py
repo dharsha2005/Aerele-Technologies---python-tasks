1248d978d4f5eeb1191828497b9cd24e3a43108a
dharsha2005 <baladharshan1972@gmail.com>
2026-07-20 15:48:16 +0530

    day5 update

--- day5/Sample tasks/testing.py
+++ day5/Sample tasks/testing.py
@@ -2,10 +2,13 @@ import unittest
 def add(x, y):
     return x + y
 class TestAdd(unittest.TestCase):
-    def test_add(self):
+    def test_positive(self):
         self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
+    def test_negative(self):
+        self.assertEqual(add(-2, -3), -5)
+    def test_zero(self):
         self.assertEqual(add(0, 0), 0)
+        
 
 
 if __name__ == '__main__':
